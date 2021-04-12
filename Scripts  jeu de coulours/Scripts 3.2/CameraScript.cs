ba227a761627fc60783c303bb14bf92d5844d3bf
elhatimi <77122877+elhatimi@users.noreply.github.com>
2021-04-12 17:48:21 +0200

    First commit

---
+++ Scripts  jeu de coulours/Scripts 3.2/CameraScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+ float Yrot;
+
+ RaycastHit hitPickup;
+
+ GameObject grabbedObj;
+
+ public Transform grabbedObjLocation;
+
+
+
+ void Update()
+
+    {
+
+        Yrot -= Input.GetAxis("Mouse Y");
+
+        Yrot = Mathf.Clamp(Yrot, -60, 60);
+
+        transform.localRotation = Quaternion.Euler(Yrot, 0f, 0f);
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hitPickup) &&     hitPickup.transform.GetComponent<Rigidbody>())
+
+        {
+
+            grabbedObj = hitPickup.transform.gameObject;
+
+        }
+
+        else if(Input.GetMouseButtonUp(0))
+
+        {
+
+            grabbedObj = null;
+
+        }
+
+
+
+        if(grabbedObj)
+
+        {
+
+            grabbedObj.GetComponent<Rigidbody>().velocity = 10* ( grabbedObjLocation.position - grabbedObj.transform.position);
+
+        }
+
+    }
+}
